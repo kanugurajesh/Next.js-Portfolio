898810bd176f002b0e37b65ef9045246c4817b1d
kanugurajesh <kanugurajesh3@gmail.com>
2024-08-06 17:05:04 +0530

    Adding completed item cards

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Section title={"Projects"} icon={<Folder />} >
-        <ItemCard image={"/projects/card.svg"} title={"company"} description={"I worked as a software engineer I worked as a software engineer"} tags={["Next.js", "shadcn/ui", "Tailwind", "React"]} href={"https://google.com"}  />
+        <ItemCard image={"/projects/card.svg"} title={"Mentify"} description={"An all in one solution for mental health issues"} tags={["Next.js", "shadcn/ui", "Tailwind", "React"]} href={"https://google.com"} downborder={true} />
+        <ItemCard image={"/projects/card.svg"} title={"Mentify"} description={"An all in one solution for mental health issues"} tags={["Next.js", "shadcn/ui", "Tailwind", "React"]} href={"https://google.com"}  />
       </Section>
     </main>
   );
--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -2,45 +2,54 @@ import React from "react";
 import Image from "next/image";
 import { Badge } from "./ui/badge";
 import Link from "next/link";
+import { ChevronRight } from "lucide-react";
 
+// Declaring the props to use it in the ItemCard component
 type Props = {
   image: string;
   title: string;
   description: string;
   tags: string[];
   href: string;
+  downborder?: boolean;
 };
 
-const ItemCard = ({
-  image,
-  title,
-  description,
-  tags,
-  href,
-}: Props) => {
+const ItemCard = ({ image, title, description, tags, href, downborder }: Props) => {
   return (
-    <Link className="flex justify-between w-full items-center h-full gap-2" href={href}>
-      <div className="flex flex-col justify-center items-center h-full">
-        <Image src={image} alt={title} width={100} height={100}/>
-      </div>
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <span className="flex">
-          <h3 className="text-lg font-semibold flex flex-wrap">{title}-</h3>
-          <span>{description}</span>
-        </span>
-        <div className="flex justify-start items-center">
-          <span className="flex justify-center items-center gap-1">
-            {tags.map((tag, index) => (
-              <Badge
-                key={tag + index}
-                className="text-xs font-semibold bg-primary-background text-primary"
-                variant="outline"
-              >
-                {tag}
-              </Badge>
-            ))}
-          </span>
+    <Link
+      className="flex justify-between w-full items-center gap-2 pr-4"
+      href={href}
+    >
+      <div className={`flex items-center gap-2 ${downborder && "py-4 border-b border-primary-background"}`}>
+        <div className="flex flex-col justify-center items-center">
+          <Image src={image} alt={title} width={120} height={120} />
         </div>
+        <div className="flex flex-col justify-center items-start gap-1">
+          <span className="flex">
+            <h3>
+              <span className="text-lg font-bold">{title}- </span>
+              {description}
+            </h3>
+          </span>
+          <div className="flex justify-start items-center">
+            <span className="flex justify-center items-center gap-1">
+              {tags.map((tag, index) => (
+                <Badge
+                  key={tag + index}
+                  className="text-xs font-bold bg-primary-background text-primary"
+                  variant="outline"
+                >
+                  {tag}
+                </Badge>
+              ))}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="p-2 hover:bg-black hover:text-white rounded-full transition-all ease-in-out duration-300">
+        <Link href={href}>
+          <ChevronRight size={24} />
+        </Link>
       </div>
     </Link>
   );
